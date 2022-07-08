3037ad35a83b505bd1a6d4e86bbc612d0f0da0ae
hassanalyhitch <hassanaly.b@gmail.com>
2022-07-08 11:49:43 +0300

    1 file modified

--- src/app/pipes/currency-format.pipe.ts
+++ src/app/pipes/currency-format.pipe.ts
@@ -1,8 +1,10 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Injectable } from '@angular/core';
 
 @Pipe({
   name: 'currencyFormat'
 })
+@Injectable({ providedIn: 'root' })
 export class CurrencyFormatPipe implements PipeTransform {
 
   transform(value: number,
