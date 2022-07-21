cc5c94d0b12e3672fa1784dbfb3d9a42a15a051b
hassanalyhitch <hassanaly.b@gmail.com>
2022-07-21 13:11:05 +0300

    4 files modified

--- src/app/main-dash/insurance/new-contract/new-contract.component.ts
+++ src/app/main-dash/insurance/new-contract/new-contract.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-new-contract',
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewContractComponent implements OnInit {
 
+  @ViewChild('startdate', { static: true }) startDate: ElementRef;
+  @ViewChild('enddate', { static: true }) endDate: ElementRef;
+  @ViewChild('startDateLabel', { static: true }) startDateLabel: ElementRef;
+  @ViewChild('endDateLabel', { static: true }) endDateLabel: ElementRef;
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onStartDateClicked(){
+    this.startDate.nativeElement.style.setProperty('color', '#212121');
+    this.startDateLabel.nativeElement.style.setProperty('color', '#009ee2');
+  }
+  onEndDateClicked(){
+    this.endDate.nativeElement.style.setProperty('color', '#212121');
+    this.endDateLabel.nativeElement.style.setProperty('color', '#009ee2');
   }
 
 }
